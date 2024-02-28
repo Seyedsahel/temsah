2f053493aeb989ac17d10044c71eae78a6466b78
taa <TahaM8000@gmail.com>
2024-02-28 09:54:50 +0330

    change output structure from dictionary to class.

--- temsah/main.py
+++ temsah/main.py
@@ -3,24 +3,41 @@ from bs4 import BeautifulSoup
 import chardet
 
 
-out_put={}
-
 #You can use the commented codes to test the code
+#---------------------------
+class Product:
+    def __init__(self,site):
+        self.name = ""
+        self.site = site
+        self.price = ""
+        self.discount = False
+        self.price_out = ""
+        self.price_in = ""
+        self.image = ""
+        self.url = ""
 
+    def __str__(self):
+        output = f"name: {self.name}\nsite: {self.site}\n"
+        if(not self.discount):
+            output += f"price: {self.price}\n"
+        output += f"discount: {self.discount}\nprice_out: {self.price_out}\nprice_in: {self.price_in}\nimage: {self.image}\nurl: {self.url}"
+        return output
 #---------------------------
 class find :
     def __init__(self,soup):
         self.soup = soup 
+        self.product = None
         
     def amazon(self):
-        soup=self.soup
+        soup = self.soup
+        self.product = Product("amazon")
         
         try:  
                 #product name
                 title=soup.find('title')
                 if title:
                     product_name=title.text[12:]
-                    out_put["product name"] = product_name.strip()
+                    self.product.name = product_name.strip()
                     # print(f"product name:{product_name}") 
                 else:
                     print('product name tag not found')
@@ -30,7 +47,7 @@ class find :
                 if div_tag:
                     img_tag = div_tag.find('img')
                     if img_tag:
-                        out_put["img src"] = img_tag['src']
+                        self.product.image = img_tag['src']
                         # print(f"img src:{img_tag['src']}")  #img src
                 else:
                     print('img tag not found')
@@ -40,19 +57,23 @@ class find :
                 if span_tag:
                     price_out=span_tag.find('span' , class_='a-offscreen')
                     if price_out: #in discount
-                        out_put["discount"] = True
-                        out_put["price_out"] = price_out.text.strip()
+
+                        self.product.price_out = price_out.text.strip()
+                        self.product.discount = True
                 
                         price_in = soup.find('span', class_='a-price aok-align-center reinventPricePriceToPayMargin priceToPay')
                         if price_in:
-                            out_put["price_in"] = price_in.text.strip()
+                            # self.product.price_in = price_in.text.strip()
+                            self.product.price_in = price_in.text.strip()
                         else:
                             print('price not found')  
                 else:
                     price = soup.find('span', class_='a-price aok-align-center reinventPricePriceToPayMargin priceToPay')
                     if price:
-                        out_put["price"] = price.text.strip()
-                        out_put["discount"] = False
+                        # self.product.price = price.text.strip()
+                        # self.product.discount = False
+                        self.product.price = price.text.strip()
+                        self.product.discount = False
                     else:
                         print('price not found')
 
@@ -61,10 +82,11 @@ class find :
             print(e)
         
     
-        #---------------------------
+    #---------------------------
 
     def nike(self):
         soup=self.soup
+        self.product = Product("nike")
         
         try:
 
@@ -72,7 +94,7 @@ class find :
             name=soup.find('span',class_='b-pdp__product-name js-gtm-product-name')
             if name:
                 product_name=name.text
-                out_put["product name"] = product_name.strip()
+                self.product.name = product_name.strip()
                     
             else:
                 print('product name tag not found')
@@ -80,7 +102,7 @@ class find :
             #img
             img_tag = soup.find('img', class_='b-picture__img b-pdpimages__carousel-img js-zoomed-img js-lazy-disabled')
             if img_tag:
-                out_put["img src"] = img_tag['src']
+                self.product.image = img_tag['src']
             else:
                 print('img tag not found')
 
@@ -88,16 +110,16 @@ class find :
 
             span=soup.find('span', class_="strike-through list")  
             if span: #discount
-                out_put["discount"] = True
+                self.product.discount = True
                 sp=span.find('span',class_='value')
-                out_put["price_out"] = sp['content'].strip()
+                self.product.price_out = sp['content'].strip()
             span_tag = soup.find('span', class_='sales')
             if span_tag:
                 sp=span_tag.find('span',class_='value')
-                if "discount" in out_put and out_put["discount"]:
-                    out_put["price_in"] = sp.text.strip()
+                if self.product.discount:
+                    self.product.price_in = sp.text.strip()
                 else:
-                    out_put["price"] = sp.text.strip()
+                    self.product.price = sp.text.strip()
 
             else:
                 print('price tag not found')
@@ -111,6 +133,7 @@ class find :
 
     def adidas(self):
         soup=self.soup
+        self.product = Product("adidas")
         
         try:
             
@@ -118,7 +141,7 @@ class find :
             name=soup.find('h1',class_='product-name')
             if name:
                 product_name=name.text
-                out_put["product name"] = product_name.strip()
+                self.product.name = product_name.strip()
             else:
                 print('product name tag not found')
 
@@ -127,7 +150,7 @@ class find :
             if div_tag:
                 img_tag = div_tag.find('img', class_='img-fluid')
                 if img_tag:
-                    out_put["img src"] = img_tag['src']
+                    self.product.image = img_tag['src']
                 else:
                     print('img tag not found')
             else:
@@ -141,12 +164,12 @@ class find :
                     price_in=span_tag.find('span',class_='value')
                     price_out=div_tag.find('span',class_='value')
                     if price_in['content'] == price_out['content'] : 
-                        out_put["price"] = price_in
-                        out_put["discount"] = False
+                        self.product.price = price_in
+                        self.product.discount = False
                     else:
-                        out_put["price_out"] = price_out
-                        out_put["price_in"] = price_in
-                        out_put["discount"] = True
+                        self.product.price_out = price_out
+                        self.product.price_in = price_in
+                        self.product.discount = True
             else:
                 print('price tag not found')
 
@@ -158,14 +181,15 @@ class find :
     #---------------------------
 
     def namshi(self):
-        soup=self.soup
+        soup = self.soup
+        self.product = Product("namshi")
         
         try:
             #product name
-            name=soup.find('h1',class_='ProductConversion_productTitle__dvlc5')
+            name = soup.find('h1',class_='ProductConversion_productTitle__dvlc5')
             if name:
                 product_name = name.text
-                out_put["product name"] = product_name.strip()
+                self.product.name = product_name.strip()
             else:
                 print('name tag not found')
 
@@ -176,7 +200,7 @@ class find :
                 img_tag = div_tag.find('img')
                 if img_tag:
                     final_src = f"https://www.namshi.com{img_tag['src']}"
-                    out_put["img src"] = final_src
+                    self.product.image = final_src
             else:
                 print('img tag not found')
 
@@ -184,18 +208,18 @@ class find :
             span1_tag = soup.find('span', class_='ProductPrice_sellingPrice__y8kib ProductPrice_xLarge__6DRdu')
             if span1_tag:
                 span2_tag = span1_tag.find('span', class_='ProductPrice_value__hnFSS')
-                out_put["discount"] = False
-                out_put["price"] = span2_tag.text
+                self.product.discount = False
+                self.product.price = span2_tag.text
             else:
-                out_put["discount"] = True
+                self.product.discount = True
                 div_tag=soup.find('div',class_='ProductPrice_preReductionPrice__S72wT') #in discount
                 if div_tag:
-                    out_put["price_out"] = div_tag.text
+                    self.product.price_out = div_tag.text
                 else:
                     print('price tag not found')
                 span_tag=soup.find('span',class_='ProductPrice_sellingPrice__y8kib ProductPrice_discounted__Puxu6 ProductPrice_xLarge__6DRdu')
                 if span_tag:
-                    out_put["price_in"] = span_tag.text
+                    self.product.price_in = span_tag.text
                 else:
                     print('price tag not found')
 
@@ -206,40 +230,43 @@ class find :
 
     #---------------------------
 
-    #product name        
+           
     def sharafdg(self):
-        soup=self.soup
+        soup = self.soup
+        self.product = Product("sharafdg")
+
+        #product name 
         try:
             name=soup.find('h1',class_='product_title entry-title')
             if name:
                 product_name=name.text
-                out_put["product name"] = product_name.strip() 
+                self.product.name = product_name.strip() 
             else:
                 print('name tag not found')
             
             #img
             img_tag = soup.find('img', class_='img-responsive elevateZoom')
             if img_tag:
-                out_put["img src"] = img_tag['src'] #img src
+                self.product.image = img_tag['src'] #img src
             else:
                 print('img tag not found')
 
             #price 
                 
-            out_put["discount"] = False
+            self.product.discount = False
             div_tag = soup.find('span', class_='strike')
             if div_tag:
-                out_put["discount"] = True
-                out_put["price_out"] = div_tag.text.strip()
+                self.product.discount = True
+                self.product.price_out = div_tag.text.strip()
             
             div_tag = soup.find('div', class_='price')
             if div_tag:
                 span_tag = div_tag.find('span', class_='total--sale-price')
                 if span_tag :
-                    if "discount" in out_put and out_put["discount"]:
-                        out_put["price_in"] = span_tag.text.strip()
+                    if self.product.discount:
+                        self.product.price_out = span_tag.text.strip()
                     else:
-                        out_put["price"] = span_tag.text.strip()
+                        self.product.price = span_tag.text.strip()
                         
                 else:
                     print('price tag not found')
@@ -252,12 +279,13 @@ class find :
     #---------------------------
     def noon(self):
         soup=self.soup
+        self.product = Product("noon")
         try:
             #product name  
             name=soup.find('h1',class_='sc-f5f69516-18 cmxvfi')
             if name:
                 product_name=name.text
-                out_put["product name"] = product_name.strip() 
+                self.product.name = product_name.strip() 
             else:
                 print('name tag not found')
 
@@ -266,7 +294,7 @@ class find :
             if div_tag:
                 img_tag = div_tag.find('img')
                 if img_tag:
-                    out_put["img src"] = img_tag['src']
+                    self.product.image = img_tag['src']
             else:
                 print('img tag not found')
 
@@ -276,12 +304,12 @@ class find :
                 price_out = div_tag.find('div', class_='priceWas')
                 price_in= div_tag.find('div',class_='priceNow')
                 if price_out: #discount
-                    out_put["discount"] = True
-                    out_put["price_out"] = price_out.text
-                    out_put["price_in"] = price_in.text
+                    self.product.discount = True
+                    self.product.price_out = price_out.text
+                    self.product.price_in = price_in.text
                 else:
-                    out_put["price"] = price_in.text
-                    out_put["discount"] = False
+                    self.product.price = price_in.text
+                    self.product.discount = False
             else:
                 print("price tag not found")
 
@@ -291,13 +319,15 @@ class find :
 
     #---------------------------
 
-    def carre(url,soup):
+    def carre(self):
+        soup = self.soup
+        self.product = Product("carre")
         try:
             #product name
             name=soup.find('h1',class_='css-106scfp')
             if name:
                 product_name=name.text
-                out_put["product name"] = product_name.strip() 
+                self.product.name = product_name.strip() 
             else:
                 print('name tag not found')
 
@@ -306,7 +336,7 @@ class find :
             if div_tag:
                 img_tag = div_tag.find('img')
                 if img_tag:
-                    out_put["img src"] = img_tag['src']
+                    self.product.image = img_tag['src']
             else:
                 print('img tag not found')
 
@@ -315,22 +345,22 @@ class find :
             if div_tag:
                 price_out = div_tag.find('span', class_='css-rmycza')
                 if price_out:
-                    out_put["price_out"] = price_out.text
-                    out_put["discount"] = True
+                    self.product.price_out = price_out.text
+                    self.product.discount = True
                     price_in= div_tag.find('h2',class_='css-1i90gmp')
                     input_string = price_in.text
                     start_index = input_string.find("AED ")  
                     end_index = input_string.find("AED ", start_index + 1)  
                     if start_index != -1 and end_index != -1:
                         modified_string = input_string[start_index:end_index]
-                        out_put["price_in"] = modified_string  
+                        self.product.price_in = modified_string  
                     else:
                         print("AED not found in the input string")   
                 else:
                     price = div_tag.find('h2', class_='css-17ctnp')
                     if price:
-                        out_put["price"] = price.text
-                        out_put["discount"] = False
+                        self.product.price = price.text
+                        self.product.discount = False
             else:
                 print('price tag not found')
 
@@ -346,7 +376,7 @@ class main :
         self.url = url
     
     def scrape(self):
-        url=self.url
+        url = self.url
         headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
         'Accept-Language': 'en-US,en;q=0.5',
@@ -357,36 +387,29 @@ class main :
             encoding = chardet.detect(page.content)["encoding"]
             html = page.content.decode(encoding)
             soup = BeautifulSoup(html, 'html.parser') 
-            product=find(soup)
+            scraper = find(soup)
     #---------------------------
 
             site_name = url[12:]
             if site_name.startswith('amazon'):
-                status='amazon'
-                product.amazon()   
+                scraper.amazon()   
             elif site_name.startswith('nike'):
-                status='nike'
-                product.nike()
+                scraper.nike()
             elif site_name.startswith('adidas'):
-                status='adidas'
-                product.adidas()
+                scraper.adidas()
             elif site_name.startswith('namshi'):
-                status='namshi'
-                product.namshi()
+                scraper.namshi()
             elif "sharafdg" in site_name:
-                status='sharafdg'
-                product.sharafdg()
+                scraper.sharafdg()
             elif site_name.startswith('noon'):
-                status='noon'
-                product.noon()
+                scraper.noon()
             elif site_name.startswith('carrefouruae'):
-                status='carrefouruae'
-                product.carre()
+                scraper.carre()
                 
 
-            out_put["site kind"] = status      
+            scraper.product.url = url
                 
-            return out_put
+            return scraper.product
 
         else:
             print(f"page.status_code:{page.status_code}")
